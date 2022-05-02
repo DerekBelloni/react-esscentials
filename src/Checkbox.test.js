cff122c3e19980c6e21a4aa5b3cbf6763d8086f7
Derek <derekbelloni10@gmail.com>
2022-05-02 11:14:33 -0600

    Ignore message intellisense

--- src/Checkbox.test.js
+++ src/Checkbox.test.js
@@ -8,5 +8,6 @@ test("Selecting checkbox", () => {
   const { getByLabelText } = render(<Checkbox />)
   const checkbox = getByLabelText(/not checked/)
   fireEvent.click(checkbox);
+  // @ts-ignore
   expect(checkbox.checked).toEqual(true);
 });
